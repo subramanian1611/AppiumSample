e28cf5455bb07945a74ca98c8822a27960350177
Nitin Thite <thitenitin@gmail.com>
2020-06-19 18:47:51 +0530

    added emulator details

--- src/main/java/base/Setup.java
+++ src/main/java/base/Setup.java
@@ -11,8 +11,6 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -82,7 +80,6 @@ public class Setup {
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
-
 		}
 		
 		return driver;
